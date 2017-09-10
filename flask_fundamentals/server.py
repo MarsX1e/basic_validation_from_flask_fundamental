a8ad39ecba90c4072cb38ca69145ab11deac2a5d
MarsX1e <tianyuxie14@gmail.com>
2017-09-10 11:53:53 -0700

    write code to check volidation. I failed because of secret Key. I still write regex. Probably try it after I get answered'

--- flask_fundamentals/server.py
+++ flask_fundamentals/server.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
+import re
+EMAIL_REGES=re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+# string's color is so strange
 app=Flask(__name__)
 app.sercret_key="KeepItSecretKeepItSafe"
+
 # does it really matter what string we put in sercret key?
-@app.route('/')
+# I failed. It says no secret is generated, but there is the key we set up at the beginning.
+@app.route('/',method=['GET'])
+# what's get method here for? Is it necessary? not other servers we build have that.
 def index():
     return render_template('index.html')
+app.sercret_key="afeafeagrewag/afe/af;eafok"
 @app.route('/process',methods=['Post'])
 def process():
+    if len(request.form['name'])<1:
+        flash("Name cannot be empty!")
+    else:
+        flash("Success! Your name is {}".format(request.form['name']))
+    return redirect('/')
+def submit():
+    if len(request.form['email'])<1:
+        flash("Email cannot be blank!")
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash('invalid Email Address!')
+    else: 
+        flash("Success")
     return redirect('/')
 app.run(debug=True)
